fa818823c7dc2912e4d38087e9b2d81290bd954f
Dan-Coelho <danpoco@gmail.com>
2023-12-20 16:40:15 -0300

    Instalação do mongoose e criação do primeiro schema e model

--- server.js
+++ server.js
@@ -1,6 +1,54 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
+const db = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('db connection sucessfull'));
+
+//criar um schema
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+//Criando documento teste
+const testTour = new Tour({
+  name: 'The Forest Hill',
+  rating: 4.0,
+  price: 422,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 //4 START SERVER
 //Iniciar um servidor
