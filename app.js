eeacb24cdaf039e4c3f73e881218b2fc3ff46ea6
Dan-Coelho <danpoco@gmail.com>
2024-01-05 14:23:29 -0300

    Tratamento de erros e alguns melhoramentos Modificação no arquivo gitignore

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
+const { dirname } = require('path');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
-const { dirname } = require('path');
+
 const app = express();
 //1-MIDDLEWARES
 //Middleware morgan
-console.log(process.env.NODE_ENV);
+//console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
@@ -14,11 +17,11 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 //Definindo nosso middleware
-app.use((req, res, next) => {
-  console.log('-----------');
-  console.log('Hello from the middleware');
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log('-----------');
+//   console.log('Hello from the middleware');
+//   next();
+// });
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
@@ -28,5 +31,15 @@ app.use((req, res, next) => {
 //MOUTING ROUTERS
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+//route for paths which don't exist
+app.all('*', (req, res, next) => {
+  // const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
